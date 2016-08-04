487fc644ebd03a757dd1df82251e1c0c192e34fa
Mark Mossberg <mark@trailofbits.com>
2016-08-03 21:57:49 -0400

    ty james

---
+++ infect.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# i don't know bash so i just took james's, go follow him github.com/semaj
+
+########## Variables
+
+echo ">> INFECTING!"
+dir=~/dots                    # dotfiles directory
+target=~
+files="vimrc vim tmux.conf gitignore gitconfig gdbinit aliases"    # list of files/folders to symlink in homedir
+
+##########
+
+for file in $files; do
+  echo "Dealing with .$file:"
+  if [[ ! -L $target/.$file ]]; then
+    echo "  Creating symlink to dots $file in home directory."
+    ln -sv $dir/.$file $target/.$file
+  else
+    echo "  Already symlinked. Doing nothing."
+  fi
+done
+
+ln -sv $dir/fish $target/.config/fish
+
+echo ">> DONE INFECTING!"
