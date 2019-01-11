267d01c805abdfb722cfd3e94474baa56f11b051
Mark Mossberg <mark@trailofbits.com>
2019-01-11 21:56:59 +0000

    ss

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,10 @@
 source ~/.aliases
 source ~/.abbr.fish
 
+fish_vi_key_bindings
+
 # env variables
-set -g -x PATH . /usr/local/bin $PATH ~/dotfiles/bin /Users/mark/Library/Python/2.7/bin
+set -g -x PATH . /usr/local/bin $PATH ~/bin ~/dotfiles/bin /Users/mark/Library/Python/2.7/bin
 set -g -x PYTHONPATH /Applications/Binary\ Ninja.app/Contents/Resources/python/
 set -g -x fish_greeting ""
 set -g -x EDITOR ~/dotfiles/e
